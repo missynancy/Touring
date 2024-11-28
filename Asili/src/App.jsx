e96d92ff8586b986c83f3352432e243163367a3b
missynancy <missynancy722@outlook.com>
2024-11-28 20:34:48 +0000

    footer

--- Asili/src/App.jsx
+++ Asili/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         
       </Routes>
+      <Footer />
     </Router>
   );
 }
